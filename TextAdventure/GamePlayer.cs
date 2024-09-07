40937e137607397e72a6f86a37bae33d845fb81b
matthew-elgart <matthewjelgart@gmail.com>
2024-09-07 17:04:32 -0400

    push stuff that was leftover

--- TextAdventure/GamePlayer.cs
+++ TextAdventure/GamePlayer.cs
@@ -22,5 +22,32 @@ namespace TextAdventure
 				Console.WriteLine(stringToPrint);
 			}
 		}
+
+		public static void WriteWrapped(string text){
+			const int maxLineLength = 80;
+			foreach (var line in GamePlayer.WordWrap(text, maxLineLength)){
+				Console.WriteLine("{0, 62} {1, 80}", "", line);
+			}
+		}
+
+		// Copied from https://gist.github.com/anderssonjohan/660952
+		private static List<string> WordWrap(string text, int maxLineLength)
+		{
+			var list = new List<string>();
+
+			int currentIndex;
+			var lastWrap = 0;
+			var whitespace = new[] { ' ', '\r', '\n', '\t' };
+			do
+			{
+				currentIndex = lastWrap + maxLineLength > text.Length ? text.Length : (text.LastIndexOfAny(new[] { ' ', ',', '.', '?', '!', ':', ';', '-', '\n', '\r', '\t' }, Math.Min(text.Length - 1, lastWrap + maxLineLength)) + 1);
+				if (currentIndex <= lastWrap)
+					currentIndex = Math.Min(lastWrap + maxLineLength, text.Length);
+				list.Add(text.Substring(lastWrap, currentIndex - lastWrap).Trim(whitespace));
+				lastWrap = currentIndex;
+			} while (currentIndex < text.Length);
+
+			return list;
+		}
 	}
 }
