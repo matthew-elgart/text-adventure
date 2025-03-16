1f96393719ebbd38a0c6bdf53624a936ac61214f
My Name <test@example.com>
2025-03-16 12:20:06 -0400

    (pushing code from pc) attempt word wrapping for output

--- TextAdventure/GamePlayer.cs
+++ TextAdventure/GamePlayer.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using TextAdventure.GameStateStuff;
 
 namespace TextAdventure
@@ -12,15 +13,47 @@ namespace TextAdventure
 		}
 		public void Play()
 		{
-			Console.WriteLine(this._gameState.CurrentLocation.GetFullLocationDescription(this._gameState.Protagonist));
+			//Console.WriteLine(this._gameState.CurrentLocation.GetFullLocationDescription(this._gameState.Protagonist));
+			GamePlayer.WriteWrapped(this._gameState.CurrentLocation.GetFullLocationDescription(this._gameState.Protagonist));
 
 			while (!this._gameState.GameIsOver)
 			{
 				var input = Program.ReadInputFromUser();
 				var command = InputParser.ParseInput(input);
 				var stringToPrint = command.ExecuteCommand(this._gameState);
-				Console.WriteLine(stringToPrint);
+				//Console.WriteLine(stringToPrint);
+				GamePlayer.WriteWrapped(stringToPrint);
 			}
+		}
+
+		public static void WriteWrapped(string text)
+		{
+			const int maxLineLength = 80;
+			foreach (var line in GamePlayer.WordWrap(text, maxLineLength))
+			{
+				//Console.WriteLine("{0, 62} {1, 80}", "", line);
+				Console.WriteLine(line);
+			}
+		}
+
+		// Copied from https://gist.github.com/anderssonjohan/660952
+		private static List<string> WordWrap(string text, int maxLineLength)
+		{
+			var list = new List<string>();
+
+			int currentIndex;
+			var lastWrap = 0;
+			var whitespace = new[] { ' ', '\r', '\n', '\t' };
+			do
+			{
+				currentIndex = lastWrap + maxLineLength > text.Length ? text.Length : (text.LastIndexOfAny(new[] { ' ', ',', '.', '?', '!', ':', ';', '-', '\n', '\r', '\t' }, Math.Min(text.Length - 1, lastWrap + maxLineLength)) + 1);
+				if (currentIndex <= lastWrap)
+					currentIndex = Math.Min(lastWrap + maxLineLength, text.Length);
+				list.Add(text.Substring(lastWrap, currentIndex - lastWrap).Trim(whitespace));
+				lastWrap = currentIndex;
+			} while (currentIndex < text.Length);
+
+			return list;
 		}
 	}
 }
